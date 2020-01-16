abca37df64e04d48c63110e28eeeb9c953777871
igoraleksejcuk <igor.aleksejcuk@neklo.com>
2020-01-16 12:49:41 +0300

    slider adaptive

--- class_work/axit/js/main.js
+++ class_work/axit/js/main.js
@@ -37,12 +37,15 @@ $(document).ready(function(){
             {
               breakpoint: 768,
               settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
                 slidesToShow: 1
               }
-            }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 2
+                }
+              }
         ]
     });
     $('.contact_form').submit(function (e) {
