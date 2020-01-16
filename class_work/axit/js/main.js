46519b7dbc93626fc1c73d5f7281964d997de808
igoraleksejcuk <igor.aleksejcuk@neklo.com>
2020-01-16 12:45:20 +0300

    slider adaptive

--- class_work/axit/js/main.js
+++ class_work/axit/js/main.js
@@ -35,32 +35,15 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
         responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 768,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
               }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
+        ]
     });
     $('.contact_form').submit(function (e) {
         e.preventDefault();
