2883d3f3e8dc7393ae2ca69124f3d8480aca2176
igoraleksejcuk <igor.aleksejcuk@neklo.com>
2020-01-24 12:08:50 +0300

    webpack project

---
+++ class_work/wp_proj/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path') 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports ={
+    entry :{
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(css|sass|less|scss)/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {sourceMap: true}
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {sourceMap: true, config: {path: 'src/js/configs/postcss.config.js'}}
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {sourceMap: true}
+                    }
+                ],
+              }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '[name].css'
+        }),
+      ],
+    devServer :{
+        overlay: true
+    }
+}
