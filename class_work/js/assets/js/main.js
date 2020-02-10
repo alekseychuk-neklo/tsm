24531190f9b4d0e319e1d03175a14da748f35896
igoraleksejcuk <igor.aleksejcuk@neklo.com>
2020-02-10 23:17:49 +0300

    js homework

--- class_work/js/assets/js/main.js
+++ class_work/js/assets/js/main.js
@@ -1,81 +1,63 @@
 var student1Obj = {
-    id:1,
+
     firstName: 'Ololo',
     lastName: 'Ololo',
-    active: true,
-    educationLevel: null
+    age: 18,
+    grades: [1,1,3,2,5],
+    course:1,
+    gender: "male"
 
 }
 var student2Obj = {
-    id:2,
+
     firstName: 'Ololo2',
-    lastName: 'Ololo',
-    active: false,
-    educationLevel: null
+    lastName: 'Ololo2',
+    age: 18,
+    grades: [1,1,3,2,3],
+    course:1,
+    gender: "male"
+
 }
 var student3Obj = {
-    id:3,
-    firstName: 'Ololo2',
-    lastName: 'Ololo',
-    active: false,
-    educationLevel: null
+
+    firstName: 'Ololo3',
+    lastName: 'Ololo3',
+    age: 32,
+    grades: [1,3,3,2,3],
+    course:1,
+    gender: "female"
+
 }
-var studArr = [];
-studArr.push(student1Obj, student2Obj, student3Obj);
+var student4Obj = {
 
-for (var i =0; i< studArr.length; i++){
-    console.log(studArr[i])
+    firstName: 'Ololo4',
+    lastName: 'Ololo4',
+    age: 18,
+    grades: [2,2,2,2,3],
+    course:1,
+    gender: "female"
+
 }
+var students =[student1Obj, student2Obj, student3Obj, student4Obj];
 
-
-
-
-
-
-
-
-
-
-class Student {
-    constructor(firstName, lastName,age=null, distance_learning=true, specification={}){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.distance_learning = distance_learning;
-        this.specification =specification;
-    }
-
-    get fullName(){
-        return this.firstName+" "+this.lastName;
-    }
-
-    get introduction(){
-        var introduction = this.fullName+'.';
-        if(this.age !== undefined && this.age !==null){
-            introduction += `\n\r ${this.age} years old.`;
+for (let i=0; i<students.length; i++){
+    if(students[i].age>17 && students[i].age<28 && students[i].gender == 'male'){
+        let sum =0;
+        for (let j=0; j<students[i].grades.length; j++ ){
+            sum+= students[i].grades[j]
         }
-        if(this.distance_learning!== undefined && this.distance_learning!==null){
-            introduction +="\n\r "+(this.distance_learning?'full-time':'distance learning')+ ' student.'
-        }
-        if(this.specification!== undefined && this.specification!==null){
-            introduction += `\n\rSpecification: ${this.specification.name}`;
-        }
-        return introduction;
+       if((sum/students[i].grades.length) <3){
+           delete students[i].grades;
+           delete students[i].course;
+           students[i].isReadyForArmy =true;
+       }
     }
 }
+var soldiers =[];
+for ( let i =0; i<students.length; i++){
+    if (students[i].isReadyForArmy){
+        soldiers.push(students[i]);
+    }
+}
+console.log(soldiers);
 
-var student = new Student('Bill', 'Gates', 56, true, {
-    name:'it consalting',
-    group:'12-itz'
-});
-var student2 = new Student('Mark', 'Zuckerberg', 34, true, {
-    name:'seo & smm',
-    group:'14-itz'
-});
-var student3 = new Student('Pavel', 'Durov', 34, false, {});
-
-var students = [student, student2, student3];
-students.forEach(element => {
-    console.log(element.introduction);
-    console.log(element)
-});
