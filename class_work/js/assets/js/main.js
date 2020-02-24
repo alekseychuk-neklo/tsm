8e2de3ba709b7b1fe45df2a71eb4eea5d8683136
igoraleksejcuk <igor.aleksejcuk@neklo.com>
2020-02-24 22:41:47 +0300

    js homework

--- class_work/js/assets/js/main.js
+++ class_work/js/assets/js/main.js
@@ -1,32 +1,73 @@
-let fruits =[
-    {name: 'banana', count: 10},
-    {name: 'orange', count: 10},
-    {name: 'apple', count: 10},
-    {name: 'lemon', count: 10},
-    {name: 'mandarin', count: 10},
+let clients = [
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 3},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 2},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 1},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 4},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 1000},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 2},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 2},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 2},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 2},
+    {name: 'String', balance: 123, age: 21, alcoholName: 'martini', alcoholCount: 2},
 ];
 
-function getArgument(arr, attr) {
-    return arr.map(function (element) {
-        return element[attr];
-    })
-}
-let tmp = getArgument(fruits, 'name');
-console.log(tmp);
-let userBooks =[
-    {name: 'Anna', books: ['Harry Potter', 'War and Peace']},
-    {name: 'Anna2', books: ['Harry Potter2', 'War and Peace2']},
-    {name: 'Anna3', books: ['Harry Potter3', 'War and Peace3']},
-    {name: 'Anna4', books: ['Harry Potter2', 'War and Peace4']},
-    {name: 'Anna5', books: ['Harry Potter5', 'War and Peace5']},
-    ];
-function getUserBooks(userBooksArr){
-    return userBooksArr.reduce(function (acc, item) {
-        return [...acc, ...item.books].reduce(function (unique, item) {
-            return unique.includes(item)?unique: [...unique, item]
-        }, []);
-    }, []);
+let alcohol ={
+    martini: 12.3,
+    vodka: 8.4,
+    bear: 2.2,
+    viskarik: 10,
+    rom: 8.2,
 }
 
-books = getUserBooks(userBooks);
-console.log(books);
+function init(){
+    clients.forEach(function (element) {
+        let canBuy =checkAge(element.age);
+        let alcoholPrice = getAlcoholByName(element.alcoholName);
+        let sum =calculatePrice(alcoholPrice, element.alcoholCount);
+        let haveMoney = haveEnoughMoney(element.balance, sum);
+        if(canBuy && haveMoney){
+            element.balance -= sum;
+            let rubles =Math.trunc(sum);
+            let cents = (sum - rubles).toFixed(2)*100;
+             console.log(`${element.name}, вот ваш заказ: ${element.alcoholName} в количестве ${element.alcoholCount} шт. С вас ${rubles} ${getWord(rubles, ['рубль', 'рубля', 'рублей'])} ${cents} ${getWord(rubles, ['копейка', 'копейки', 'копеек'])}` )
+        }else {
+            console.log('Не пробам!!!!!!!')
+        }
+
+    })
+}
+
+
+function checkAge(age){
+    return parseInt(age)>=18;
+}
+
+function getAlcoholByName(name){
+    return alcohol[name];
+}
+
+function calculatePrice(price, count){
+        return (price * count).toFixed(2);
+}
+
+function haveEnoughMoney(balance, sum){
+    return balance>=sum;
+}
+
+function getWord(digit, words) {
+    digit = Math.abs(digit) % 100;
+    let modulo = digit % 10;
+    if (digit > 10 && digit < 20) {
+        return words[2];
+    }
+    if (modulo > 1 && modulo < 5) {
+        return words[2];
+    }
+    if (modulo == 1) {
+        return words[0];
+    }
+    return words[2];
+}
+
+init();
+
